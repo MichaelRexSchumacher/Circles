6fe34b698f502458c5831bb8a17a6e7e9717b50f
mschumac <Michael_Schumacher@Optum.com>
2022-01-02 15:44:01 -0600

    HNY

--- sketch.js
+++ sketch.js
@@ -37,93 +37,90 @@ function draw() {
   //generation 1 should have 4. (1X4) 2 families
   //generation 2 should have 16 (4x4). 8 families
   //generation 3 should have 64 (16x4). 32 families
-let maxFams;
+  let maxFams;
 
- // for (let i = 0; i < 100; i++) {
-    // print("i: " + i);
-    //let gen = 0;
-    for (let gen = 0; gen < maxGen;) {
+  for (let gen = 0; gen < maxGen;) {
     print("Gen: " + gen)
     print("MaxGen: " + maxGen)
 
-      if (gen > 0) {
-        print("gen>0: " + gen);
-        maxFams = (gen * maxChildren) / 2;
-        } else {
-        maxFams = 1;
-      }
-      print("maxFams: " + maxFams);
+    //calculate the number of families that can be used for this generation.
+    if (gen > 0) {
+      maxFams = (gen * maxChildren) / 2;
+    } else {
+      maxFams = 1;
+    }
+    print("maxFams: " + maxFams);
 
-     for (let fams = 0; fams < maxFams;) {
-     print("Fams: " + fams);
+    for (let fam = 0; fam < maxFams;) {
+      print("Fam: " + fam);
 
-        let parentGenetics = getParentGenetics(gen);
+      let parentGenetics = getParentGenetics(gen);
 
-        let idX = parentGenetics.idX;
-        let idY = parentGenetics.idY;
-        print("New ParentXID: " + idX);
-        print("New ParentXID: " + idY);
+      let idX = parentGenetics.idX;
+      let idY = parentGenetics.idY;
+      print("New Parent! idx: " + idX);
+      print("New Parent! idY: " + idY);
 
 
 
-       for (let child = 0; child < maxChildren; child++) {
-       print("Child: " + child);
-          let childGenetics = calculateChildGenetics(parentGenetics);
+      for (let child = 0; child < maxChildren; child++) {
+        print("Child: " + child);
+        let childGenetics = calculateChildGenetics(parentGenetics);
 
-          //apply the genetics from the child.
-          h = childGenetics.hC;
-          s = childGenetics.sC;
-          l = childGenetics.lC;
-          sw = childGenetics.swC;
-          largeCircleSize = canvasSize - (sw * 2);
-          smallCircleSize = childGenetics.sz;
-          // posX = childGenetics.posX;
-          // posY = childGenetics.posY;
-          xPosC = childGenetics.xPosC;
-          yPosC = childGenetics.yPosC;
+        //apply the genetics from the child.
+        h = childGenetics.hC;
+        s = childGenetics.sC;
+        l = childGenetics.lC;
+        sw = childGenetics.swC;
+        largeCircleSize = canvasSize - (sw * 2);
+        smallCircleSize = childGenetics.sz;
+        // posX = childGenetics.posX;
+        // posY = childGenetics.posY;
+        xPosC = childGenetics.xPosC;
+        yPosC = childGenetics.yPosC;
 
-          ///////////////////////
-          largeCircleHue = h;
-          strokeHue = (h + 150) % 360;
-          stroke(strokeHue, s, l);
-          drawCircles(largeCircleHue, s, l, xPos, yPos, largeCircleSize, sw);
+        ///////////////////////
+        largeCircleHue = h;
+        strokeHue = (h + 150) % 360;
+        stroke(strokeHue, s, l);
+        drawCircles(largeCircleHue, s, l, xPos, yPos, largeCircleSize, sw);
 
-          //small circle
-          smallCircleHue = (h + 210) % 360;
-          drawCircles(smallCircleHue, s, l, xPosC, yPosC, smallCircleSize, 0);
+        //small circle
+        smallCircleHue = (h + 210) % 360;
+        drawCircles(smallCircleHue, s, l, xPosC, yPosC, smallCircleSize, 0);
 
-          //save the data to the table
-          let newID = max(table.getColumn("CircleID")) + 1;
-          let newRow = table.addRow();
-          newRow.setString("CircleID", newID);
-          newRow.setString("Generation", (gen + 1));
-          newRow.setString("H", h);
-          newRow.setString("S", s);
-          newRow.setString("L", l);
-          newRow.setString("StrokeWeight", sw);
-          newRow.setString("Size", smallCircleSize);
-          newRow.setString("xPosition", xPosC);
-          newRow.setString("yPosition", yPosC);
-          newRow.setString("ParentXID", idX);
-          newRow.setString("ParentYID", idY);
+        //save the data to the table
+        let newID = max(table.getColumn("ID")) + 1;
+        let newRow = table.addRow();
+        newRow.setString("ID", newID);
+        newRow.setString("Generation", (gen + 1));
+        newRow.setString("H", h);
+        newRow.setString("S", s);
+        newRow.setString("L", l);
+        newRow.setString("StrokeWeight", sw);
+        newRow.setString("Size", smallCircleSize);
+        newRow.setString("xPosition", xPosC);
+        newRow.setString("yPosition", yPosC);
+        newRow.setString("ParentXID", idX);
+        newRow.setString("ParentYID", idY);
 
-          print("newID: " + newID);
-          //save the canavas
-          //save("circle_" + newID + "_X" + idX + "_Y" + idY + ".png")
-          clear();
-          background(0, 0, 0); //reset the background to black
+        print("Child is born: " + newID);
+        //save the canavas
+        //save("ID_" + newID + "_X" + idX + "_Y" + idY + ".png")
+        clear();
+        background(0, 0, 0); //reset the background to black
 
-        }//End of the children
+      } //End of the children
 
-      fams = fams + 1;
-      }//end of families
+      fam = fam + 1; //advance the family count
+
+    } //end of families
 
     gen = gen + 1; //advance the generation
 
-   }//end of the generation
-// }
-  
-  
+  } //end of the generation
+
+
   //Once all the new rows have been added..
   saveTable(table, "genetics.csv");
 
@@ -137,186 +134,204 @@ function drawCircles(h, s, l, xloc, yloc, size, sw) {
 
 }
 
+function getParents(rows, parentArray) {
+  let xRowID = 0;
+  let yRowID = 0;
+  let parentXID;
+  let parentYID;
+  let parentExists = 0;
 
+debugger;
 
-function getParentGenetics(generation) {
-  //Find 2 random parents and get their genetics
-  //Get the rowcount of records in that generation
-  print("Generation: " + generation);
-  let rows = table.matchRows(generation, "Generation");
-  let rowcount = rows.length;
-  print("rowcount: " + rowcount);
+  print("rows: " + rows.length);
+  //get parent x
+ 
+    while (parentExists < 1){ 
+    xRowID = round(random(0, (rows.length) - 1));
+    print("xRowID: " + xRowID);
+    parentXID = rows[xRowID].get("ID");
+    if (parentArray.includes(parentXID)) {
+      parentExists = 1;
+    }
+    else{
+      parentExists = 0;
+    }
+ 
+  }
 
-  //Get a random row from the rows
-  //Get the CircleID of that row (this is the Circle ID)
-
-  let p1 = round(random(0, rowcount - 1));
-  let pID = rows[p1].get("CircleID");
-
-  //Confirm the parent has not already been chosen. How?
-  let pIDExists = false;
-  //If p1 is in the table for the next generation, then retry.
-  //First time through, there will be no next generation records. Check rowcount.
-
-  print("pID: " + pID);
-  let pXrows = table.matchRows(pID, "ParentXID");
-  let pYrows = table.matchRows(pID, "ParentYID")
-
-  print("pXrows Count: " + pXrows.length);
-  print("pYrows Count: " + pYrows.length);
-
-  if (pXrows.length > 0) {
-     pIDExists = true;
-     print("pIDExists:" + pIDExists)
+ 
   
+  parentExists = 0;
+  //get parent y
+  while (parentExists < 1 ){
+    yRowID = round(random(0, (rows.length) - 1));
+    print("yRowID: " + yRowID);
+    parentYID = rows[yRowID].get("ID");
+    if (parentYID = parentXID || parentArray.includes(parentYID)){
+      parentExists = 1;
+    }
+    else{
+      parentExists = 0;
+    }
+
+  }
+
+  return {
+      //Return the ROW ID of the parents (not the parent id)
+    xRowID: xRowID,
+    yRowID: yRowID
+
     }
 
 
+  } //choose parent end
 
-  // for (let r = 0; r < newrows.length; r++) {
+  function getParentGenetics(generation) {
+    //Find 2 new random parents and get their genetics.
+    //Ensure parents have not been used before.
 
-  //       if (pID =  newrows[r].get('CircleID')){
-  //         pIDcheck = true;
-  //       }
+    //Get the rowcount of records in that generation
+    let rows = table.matchRows(generation, "Generation");
+    let parentXArray = table.getColumn("ParentXID");
+    let parentYArray = table.getColumn("ParentYID");
+    let parentArray = parentXArray.concat(parentYArray);
 
-  // }
+    //Get the of the lucky parents
+    let parents = getParents(rows, parentArray);
+    
+    xRowID = parents.xRowID;
+    yRowID = parents.yRowID;
 
+    idX = rows[xRowID].getString("ID");
+    idY = rows[yRowID].getString("ID");
 
-  //Get the row of parent 2 (Y)... Can't be the same as Parent 1, so need to compare the random number generated
-  let p2 = p1;
-  let x = 0;
-  while (p1 === p2) {
-    p2 = round(random(0, rowcount - 1));
-  }//end while
+    hX = Number(rows[xRowID].getString("H"));
+    hY = Number(rows[yRowID].getString("H"));
 
+    sX = Number(rows[xRowID].getString("S"));
+    sY = Number(rows[yRowID].getString("S"));
 
-  idX = rows[p1].getString("CircleID");
-  idY = rows[p2].getString("CircleID");
+    lX = Number(rows[xRowID].getString("L"));
+    lY = Number(rows[yRowID].getString("L"));
 
-  hX = Number(rows[p1].getString("H"));
-  hY = Number(rows[p2].getString("H"));
+    swX = Number(rows[xRowID].getString("StrokeWeight"));
+    swY = Number(rows[yRowID].getString("StrokeWeight"));
 
-  sX = Number(rows[p1].getString("S"));
-  sY = Number(rows[p2].getString("S"));
+    szX = Number(rows[xRowID].getString("Size"));
+    szY = Number(rows[yRowID].getString("Size"));
 
-  lX = Number(rows[p1].getString("L"));
-  lY = Number(rows[p2].getString("L"));
+    xPosX = Number(rows[xRowID].getString("xPosition"));
+    xPosY = Number(rows[yRowID].getString("yPosition"));
 
-  swX = Number(rows[p1].getString("StrokeWeight"));
-  swY = Number(rows[p2].getString("StrokeWeight"));
+    yPosX = Number(rows[xRowID].getString("xPosition"));
+    yPosY = Number(rows[yRowID].getString("yPosition"));
 
-  szX = Number(rows[p1].getString("Size"));
-  szY = Number(rows[p2].getString("Size"));
+    return {
 
-  xPosX = Number(rows[p1].getString("xPosition"));
-  xPosY = Number(rows[p2].getString("yPosition"));
+      hX: hX,
+      hY: hY,
+      sX: sX,
+      sY: sY,
+      lX: lX,
+      lY: lY,
+      swX: swX,
+      swY: swY,
+      szX: szX,
+      szY: szY,
+      xPosX: xPosX,
+      yPosX: yPosX,
+      xPosY: xPosY,
+      yPosY: yPosY,
+      idX: idX,
+      idY: idY
 
-  yPosX = Number(rows[p1].getString("xPosition"));
-  yPosY = Number(rows[p2].getString("yPosition"));
+    };
 
-  return {
-
-    hX: hX,
-    hY: hY,
-    sX: sX,
-    sY: sY,
-    lX: lX,
-    lY: lY,
-    swX: swX,
-    swY: swY,
-    szX: szX,
-    szY: szY,
-    xPosX: xPosX,
-    yPosX: yPosX,
-    xPosY: xPosY,
-    yPosY: yPosY,
-    idX: idX,
-    idY: idY
-
-  };
-
-}//end GetParentGenetics
-
-
-function calculateChildGenetics(parentGenetics) {
-  //Calculate the Child Genetics. This is the math shit...
-
-  //Hue, Satuaration, Luminosity
-  hC = random(parentGenetics.hX, parentGenetics.hY);
-  sC = random(parentGenetics.sX, parentGenetics.sY);
-  lC = random(parentGenetics.lX, parentGenetics.lY);
+  }//end GetParentGenetics
 
 
-  //Stroke weight of Large Circle
-  swC = random(swX, swY);
+  function calculateChildGenetics(parentGenetics) {
+    //Calculate the Child Genetics. This is the math shit...
 
-  //Small circle size
-  sz = random(parentGenetics.szX, parentGenetics.szY);
-
-  smallCirclePositon = getSmallCirclePosition(sz, parentGenetics);
-  xPosC = smallCirclePositon.xPosC;
-  yPosC = smallCirclePositon.yPosC;
+    //Hue, Satuaration, Luminosity
+    hC = random(parentGenetics.hX, parentGenetics.hY);
+    sC = random(parentGenetics.sX, parentGenetics.sY);
+    lC = random(parentGenetics.lX, parentGenetics.lY);
 
 
-  return {
+    //Stroke weight of Large Circle
+    swC = random(swX, swY);
 
-    hC: hC,
-    sC: sC,
-    lC: lC,
-    swC: swC,
-    xPosC: xPosC,
-    yPosC: yPosC,
-    sz: sz
+    //Small circle size
+    sz = random(parentGenetics.szX, parentGenetics.szY);
+
+    smallCirclePositon = getSmallCirclePosition(sz, parentGenetics);
+    xPosC = smallCirclePositon.xPosC;
+    yPosC = smallCirclePositon.yPosC;
 
 
+    return {
+
+      hC: hC,
+      sC: sC,
+      lC: lC,
+      swC: swC,
+      xPosC: xPosC,
+      yPosC: yPosC,
+      sz: sz
+
+
+    }
+  }  //end calculateChildGenetics
+
+  function getSmallCirclePosition(sz, parentGenetics) {
+
+    //Small circle position
+    //Using Pythagores with LCR - SMR - Half the stroke weight. Stroke weight is centered on the edge.
+    //Need the Large Circle Radius
+    lcr = (canvasSize - (swC * 2)) / 2; //Canvas size - (Child strokeweight x 2) / 2
+    scr = (sz / 2);
+    let xPosC = 0;
+    let yPosC = 0;
+
+
+    let i = false;
+    while (i === false) {
+
+      xPosC = random(parentGenetics.xPosX, parentGenetics.xPosY);
+      yPosC = random(parentGenetics.yPosX, parentGenetics.yPosY);
+
+      a2 = Math.pow((xPosC - xPos), 2);
+      b2 = Math.pow((yPosC - yPos), 2);
+      ab = (Math.pow((xPosC - xPos), 2)) + (Math.pow((yPosC - yPos), 2));
+      r2 = Math.pow((lcr - scr - (swC / 2)), 2);
+
+      // print("xPos: " + xPos);
+      // print("yPos: " + yPos);
+      // print("xPosC: " + xPosC);
+      // print("xPosC: " + yPosC);
+      // print("lcr: " + lcr);
+      // print("scr: " + scr);
+      // print("a2: " + a2);
+      // print("b2: " + b2);
+      // print("ab: " + ab);
+
+      //need to figure out if the circle position ovelaps the other circle. If so, try again.
+      if (ab < r2) { i = true; }
+
+    }//end the for loop
+
+    return {
+
+      xPosC: xPosC,
+      yPosC: yPosC
+
+    }
   }
-}  //end calculateChildGenetics
-
-function getSmallCirclePosition(sz, parentGenetics) {
-
-  //Small circle position
-  //Using Pythagores with LCR - SMR - Half the stroke weight. Stroke weight is centered on the edge.
-  //Need the Large Circle Radius
-  lcr = (canvasSize - (swC * 2)) / 2; //Canvas size - (Child strokeweight x 2) / 2
-  scr = (sz / 2);
-  let xPosC = 0;
-  let yPosC = 0;
 
 
-  let i = false;
-  while (i === false) {
-
-    xPosC = random(parentGenetics.xPosX, parentGenetics.xPosY);
-    yPosC = random(parentGenetics.yPosX, parentGenetics.yPosY);
-
-    a2 = Math.pow((xPosC - xPos), 2);
-    b2 = Math.pow((yPosC - yPos), 2);
-    ab = (Math.pow((xPosC - xPos), 2)) + (Math.pow((yPosC - yPos), 2));
-    r2 = Math.pow((lcr - scr - (swC / 2)), 2);
-
-    // print("xPos: " + xPos);
-    // print("yPos: " + yPos);
-    // print("xPosC: " + xPosC);
-    // print("xPosC: " + yPosC);
-    // print("lcr: " + lcr);
-    // print("scr: " + scr);
-    // print("a2: " + a2);
-    // print("b2: " + b2);
-    // print("ab: " + ab);
-
-    //need to figure out if the circle position ovelaps the other circle. If so, try again.
-    if (ab < r2) { i = true; }
-
-  }//end the for loop
-
-  return {
-
-    xPosC: xPosC,
-    yPosC: yPosC
-
-  }
 
 
-}
+
 
 
